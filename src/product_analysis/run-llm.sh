9f49fbef631ba5f709af208b94101f5f6f6c9454
catherinehee <111953841+catherinehee@users.noreply.github.com>
2024-10-24 18:24:04 -0700

    sorry kevin

---
+++ src/product_analysis/run-llm.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Variables
+model_name="ollama/llama3:8b"
+custom_model_name="llama3"
+
+# Get the base model
+ollama pull $model_name
+
+# Create the model file
+ollama create $custom_model_name -f ./Llama3Modelfile
